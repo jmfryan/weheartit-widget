d39c8eecb9140b7c21b9c62154f479eef902be5e
Jack Ryan <jryan59@googlemail.com>
2012-02-18 16:40:56 +0000

    Add integration to the env port variable for heroku.

--- app.js
+++ app.js
@@ -46,5 +46,5 @@ function compileMethod(str) {
 app.get('/', routes.index);
 app.get('/widget/:userid', routes.widget);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
